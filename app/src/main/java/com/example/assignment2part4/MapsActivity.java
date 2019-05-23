2f83cf83f6171e0caa69fe209aa3c9c0f0a88ed7
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-23 17:20:09 +1200

    Added string request for webcam locations around current location

--- app/src/main/java/com/example/assignment2part4/MapsActivity.java
+++ app/src/main/java/com/example/assignment2part4/MapsActivity.java
@@ -13,7 +13,12 @@ import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -25,6 +30,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import org.json.JSONObject;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -64,8 +80,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }catch(SecurityException e){
             Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
         }
-        String url = Uri.parse("https://wordsapiv1.p.mashape.com/words/"+wordSearch+
-                "/definitions").buildUpon().toString();
+        String url = Uri.parse("https://webcams/list/nearby="+currentLat+","
+                +currentLng+",60").buildUpon().toString();
+        StringRequest stRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String resp = response;
+                try{
+                    JsonObject respJson = jsonFromString(resp);
+                    JsonObject respJsonResults = respJson.getJsonObject("results");
+                    JsonArray respJsonArray = respJsonResults.getJsonArray("webcams");
+                    for (int index = 0; index < (respJsonArray.size()-1);index++){
+                        JsonObject webcam = respJsonArray.getJsonObject(index);
+                        JsonObject locations = webcam.getJsonObject("location");
+                        JsonObject lat = locations.getJsonObject("latitude");
+                        JsonObject lng = locations.getJsonObject("longitude");
+                    }
+                }catch (Throwable t){
+                    Log.e("Error","Could not parse");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error",error.toString());
+            }
+        }){
+            @Override
+            public Map<String,String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("X-Mashape-Key","dd1d560155msh2a3a50aa39ffde2p13e582jsne6443e91eb58");
+                params.put("Accept","text/plain");
+                return params;
+            }
+
+        };
+        queue.add(stRequest);
+    }
+    private static JsonObject jsonFromString(String jsonObjectStr) {
+        JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
+        JsonObject object = jsonReader.readObject();
+        jsonReader.close();
+        return object;
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
