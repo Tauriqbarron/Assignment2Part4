f407dd7b07d636c991c8eedcb7db88305586f075
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-23 15:41:03 +1200

    Added location request  to on click listiner

--- app/src/main/java/com/example/assignment2part4/MapsActivity.java
+++ app/src/main/java/com/example/assignment2part4/MapsActivity.java
@@ -1,31 +1,72 @@
 package com.example.assignment2part4;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Button;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private Button btnGetWebcams;
+    private FusedLocationProviderClient fClient;
+    private int requestLocation = 99;
+    private String TAG = "Map App";
+    private Double currentLat,currentLng;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        getWebcams(this);
     }
-
-
+    public void getWebcams(Context context){
+        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        btnGetWebcams = findViewById(R.id.btnGetWebcams);
+        fClient = LocationServices.getFusedLocationProviderClient(context);
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION},requestLocation);
+        }
+        try{
+            final Task<Location> currentLocation =fClient.getLastLocation();
+            currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    currentLat = currentLocation.getResult().getLatitude();
+                    currentLng = currentLocation.getResult().getLatitude();
+                }
+            });
+        }catch(SecurityException e){
+            Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
+        }
+        String url = Uri.parse("https://wordsapiv1.p.mashape.com/words/"+wordSearch+
+                "/definitions").buildUpon().toString();
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
