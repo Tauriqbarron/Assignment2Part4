187ed47d1070c44e4b1e4f05b3dcc66d61357391
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-28 19:06:15 +1200

    created request update in on create. Edited url and Json parsing. Added markers to map

--- app/src/main/java/com/example/assignment2part4/MapsActivity.java
+++ app/src/main/java/com/example/assignment2part4/MapsActivity.java
@@ -11,7 +11,9 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -21,6 +23,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -57,74 +62,106 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fClient = LocationServices.getFusedLocationProviderClient(this);
         getWebcams(this);
+
+        LocationRequest req = new LocationRequest();
+        req.setInterval(2000);
+        req.setFastestInterval(500);
+        req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        try{
+            fClient.requestLocationUpdates(req,new LocationCallback(){
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                        currentLat = locationResult.getLastLocation().getLatitude();
+                        currentLng = locationResult.getLastLocation().getLongitude();
+                }
+            },null);
+        }catch(SecurityException e){
+            Log.e("errror",e.toString());
+        }
+
+
     }
-    public void getWebcams(Context context){
-        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-        btnGetWebcams = findViewById(R.id.btnGetWebcams);
-        fClient = LocationServices.getFusedLocationProviderClient(context);
-        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+    public void getWebcams(final Context context){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MapsActivity.this,new String[]{
+            ActivityCompat.requestPermissions(this,new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION},requestLocation);
         }
-        try{
-            final Task<Location> currentLocation =fClient.getLastLocation();
-            currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                    currentLat = currentLocation.getResult().getLatitude();
-                    currentLng = currentLocation.getResult().getLatitude();
-                }
-            });
-        }catch(SecurityException e){
-            Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
-        }
-        String url = Uri.parse("https://webcams/list/nearby="+currentLat+","
-                +currentLng+",60").buildUpon().toString();
-        StringRequest stRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        btnGetWebcams = findViewById(R.id.btnGetWebcams);
+        btnGetWebcams.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(String response) {
-                String resp = response;
-                try{
-                    JsonObject respJson = jsonFromString(resp);
-                    JsonObject respJsonResults = respJson.getJsonObject("results");
-                    JsonArray respJsonArray = respJsonResults.getJsonArray("webcams");
-                    for (int index = 0; index < (respJsonArray.size()-1);index++){
-                        JsonObject webcam = respJsonArray.getJsonObject(index);
-                        JsonObject locations = webcam.getJsonObject("location");
-                        JsonObject lat = locations.getJsonObject("latitude");
-                        JsonObject lng = locations.getJsonObject("longitude");
-                    }
-                }catch (Throwable t){
-                    Log.e("Error","Could not parse");
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("error",error.toString());
-            }
-        }){
-            @Override
-            public Map<String,String> getHeaders() throws AuthFailureError {
-                Map<String,String> params = new HashMap<>();
-                params.put("X-Mashape-Key","dd1d560155msh2a3a50aa39ffde2p13e582jsne6443e91eb58");
-                params.put("Accept","text/plain");
-                return params;
-            }
+            public void onClick(View v) {
+                LatLng moveCam = new LatLng(currentLat,currentLng);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(moveCam,14));
+/*                try{
+                    final Task<Location> currentLocation = fClient.getLastLocation();
+                    currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
 
-        };
-        queue.add(stRequest);
+                            currentLat = (double)currentLocation.getResult().getLatitude();
+                            currentLng = (double)currentLocation.getResult().getLongitude();
+                        }
+                    });
+                }catch(SecurityException e){
+                    Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
+                }*/
+                Toast.makeText(MapsActivity.this,"Assigning LatLng to Url",
+                        Toast.LENGTH_LONG).show();
+                String strLat = Double.toString(currentLat) ;
+                String strlng = Double.toString(currentLng);
+                final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = Uri.parse("https://webcamstravel.p.mashape.com/webcams/list/nearby="+strLat+","
+                        +strlng+",5?show=webcams:location").buildUpon().toString();
+                StringRequest stRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        String resp = response;
+                        try{
+                            JsonObject respJson = jsonFromString(resp);
+                            JsonObject respJsonResults = respJson.getJsonObject("result");
+                            JsonArray respJsonArray = respJsonResults.getJsonArray("webcams");
+                            for (int index = 0; index < (respJsonArray.size()-1);index++){
+                                JsonObject webcam = respJsonArray.getJsonObject(index);
+                                JsonObject curLocation = webcam.getJsonObject("location");
+                                Double lat = curLocation.getJsonNumber("latitude").doubleValue();
+                                Double lng = curLocation.getJsonNumber("longitude").doubleValue();
+                                LatLng curmarker = new LatLng(lat,lng);
+                                mMap.addMarker(new MarkerOptions().position(curmarker));
+                            }
+                        }catch (Throwable t){
+                            Log.e("Error","Could not parse");
+                        }
+                        System.err.println(response);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("error",error.toString());
+                    }
+                }){
+                    @Override
+                    public Map<String,String> getHeaders() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<>();
+                        params.put("X-Mashape-Key","dd1d560155msh2a3a50aa39ffde2p13e582jsne6443e91eb58");
+                        params.put("Accept","text/plain");
+                        return params;
+                    }
+                };
+                queue.add(stRequest);
+            }
+        });
     }
     private static JsonObject jsonFromString(String jsonObjectStr) {
         JsonReader jsonReader = Json.createReader(new StringReader(jsonObjectStr));
         JsonObject object = jsonReader.readObject();
         jsonReader.close();
         return object;
-    }
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
     }
 }
